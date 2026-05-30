fdd93fe8353f62c2918ce92f754242bf300eb797
Lauretta Maria Gullì <gruppokosmos00@gmail.com>
2026-05-30 17:47:18 +0200

    impronta + nfc

--- app/Jobs/ProcessScheduledPayments.php
+++ app/Jobs/ProcessScheduledPayments.php
@@ -4,16 +4,10 @@ namespace App\Jobs;
 
 use App\Models\ScheduledPayment;
 use App\Services\ScheduledPaymentService;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class ProcessScheduledPayments implements ShouldQueue
+class ProcessScheduledPayments
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public function handle(ScheduledPaymentService $service): void
     {
