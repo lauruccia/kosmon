f48b335e2c59f2cefd2f35cd12fc046151bc6a82
Lauretta Maria Gullì <gruppokosmos00@gmail.com>
2026-06-01 16:23:24 +0200

    fixvari2

--- routes/web.php
+++ routes/web.php
@@ -706,5 +706,7 @@ Route::get('/admin/contratto/firme/{signature}/pdf', [AdminController::class, 'c
     Route::get('/nfc-cards/{uuid}/attiva', [NfcCardController::class, 'activateForm'])->name('portal.nfc-cards.activate');
     Route::post('/nfc-cards/{uuid}/attiva', [NfcCardController::class, 'activate'])->name('portal.nfc-cards.activate.post');
     Route::post('/nfc-cards/{uuid}/limiti', [NfcCardController::class, 'updateLimits'])->name('portal.nfc-cards.limits');
+    Route::post('/nfc-cards/{uuid}/blocca', [NfcCardController::class, 'block'])->name('portal.nfc-cards.block');
+    Route::post('/nfc-cards/{uuid}/sblocca', [NfcCardController::class, 'unblock'])->name('portal.nfc-cards.unblock');
 
 });
